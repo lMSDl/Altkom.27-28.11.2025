4174797734cc6a8dc8d348c0b4a8d6a7553cb6ec
Paweł <pawel@msd.info.pl>
2025-11-27 14:28:31 +0100

    Przekazywanie parametrów

---
+++ WebAppMVC/Controllers/DataController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index(string? name, int? age)
+        {
+            //przekazanie danych do widoku za pomocą ViewData i ViewBag
+            //ViewData jest słownikiem klucz-wartość, a ViewBag jest dynamicznym obiektem
+            ViewData["name"] = name ?? "Guest";
+            ViewBag.Age = age.HasValue ? age.ToString() : "unknown";
+
+
+            /*if (TempData["message"] == null)
+                TempData["message"] = "TempData from Index";*/
+
+
+            //instrukcja zachowująca dane w TempData po odczytaniu ich w widoku
+            TempData.Keep("TempDataKeep");
+            return View();
+        }
+
+        public IActionResult Index2(string? name, int? age)
+        {
+            ViewData["name"] = name ?? "Guest";
+            ViewBag.Age = age.HasValue ? age.ToString() : "unknown";
+            /*if (TempData["message"] == null)
+                TempData["message"] = "TempData from Index";*/
+
+            return View("index");
+        }
+
+
+        public IActionResult Redirect(string? name, int? age)
+        {
+            ViewData["name"] = name ?? "Guest";
+            ViewBag.Age = age.HasValue ? age.ToString() : "unknown";
+
+            //przekazanie danych do widoku za pomocą TempData
+            //TempData przechowuje dane przez jedną redirekcję
+            TempData["message"] = "TempData from Redirect";
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult TempDataKeep()
+        {
+            TempData["TempDataKeep"] = "TempData from TempDataKeep";
+
+            return RedirectToAction(nameof(TempDataStep));
+        }
+
+        public IActionResult TempDataStep()
+        {
+            //odczytanie danych z TempData bez usuwania ich
+            var peeked = TempData.Peek("TempDataKeep");
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
