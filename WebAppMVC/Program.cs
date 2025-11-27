c7f66ff52d231687c991b87e732e846bb6d202ef
Paweł <pawel@msd.info.pl>
2025-11-27 10:29:19 +0100

    Static files

--- WebAppMVC/Program.cs
+++ WebAppMVC/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,12 +17,43 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//middleware w³¹czaj¹cy domyœlne pliki jak index.html
+//app.UseDefaultFiles(); 
+
+//middleware w³¹czaj¹cy statyczne pliki z wwwroot
+/*app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider( Path.Combine(Directory.GetCurrentDirectory(), "public")),
+    RequestPath = "/public", //zmienia domyœln¹ œcie¿kê dostêpu do plików statycznych
+    OnPrepareResponse = x => x.Context.Response.Headers.Add("Cache-Control", "public,max-age=60000") //ustawienie nag³ówków odpowiedzi HTTP
+});*/
+
+//app.UseDirectoryBrowser(); //przegl¹darka plików w formie html
+/*app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "public")),
+    RequestPath = "/public", //zmienia domyœln¹ œcie¿kê dostêpu do plików statycznych
+});
+*/
+
+//³¹czy w sobie trzy powy¿sze middleware i daje mo¿liwoœæ konfiguracji za pomoc¹ pojecynczego obiektu
+//app.UseFileServer(new FileServerOptions() { EnableDirectoryBrowsing = true }); 
+/*var options = new FileServerOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "public")),
+    RequestPath = "/public", //zmienia domyœln¹ œcie¿kê dostêpu do plików statycznych
+    EnableDirectoryBrowsing = true,
+};
+options.StaticFileOptions.OnPrepareResponse = x => x.Context.Response.Headers.Add("Cache-Control", "public,max-age=60000");
+app.UseFileServer(options);*/
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+
+app.MapStaticAssets(); //endpoint
 
 app.MapControllerRoute(
     name: "default",
