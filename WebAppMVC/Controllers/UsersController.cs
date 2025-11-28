63ccb3c5542e644742ba5046ca943a495be1647f
Paweł <pawel@msd.info.pl>
2025-11-28 12:52:37 +0100

    Dodawanie

--- WebAppMVC/Controllers/UsersController.cs
+++ WebAppMVC/Controllers/UsersController.cs
@@ -68,13 +68,25 @@ namespace WebAppMVC.Controllers
             return View(user);
         }
 
-        [HttpPost]
-        //public async Task<IActionResult> EditUser(int id, string name, string password) //przykład bez użycia atrybutów do mapowania danych z formularza
-        //public async Task<IActionResult> EditUser(int id, string name, [FromForm(Name = "password")] string pass) //przykład użycia atrybutu FromForm do mapowania nazwy pola formularza na inny parametr metody
-        //public async Task<IActionResult> EditUser(User user) //przykład mapowania całego modelu
-        public async Task<IActionResult> EditUser(int id, [Bind("Name", "Password")] User user) //przykład użycia atrybutu Bind do ograniczenia mapowania tylko do wybranych właściwości modelu
+        public IActionResult Add()
         {
-            await _service.UpdateAsync(user.Id, user);
+            return View("Edit", new User());
+        }
+
+        [HttpPost]
+        //public async Task<IActionResult> AddOrEditUser(int id, string name, string password) //przykład bez użycia atrybutów do mapowania danych z formularza
+        //public async Task<IActionResult> AddOrEditUser(int id, string name, [FromForm(Name = "password")] string pass) //przykład użycia atrybutu FromForm do mapowania nazwy pola formularza na inny parametr metody
+        //public async Task<IActionResult> AddOrEditUser(User user) //przykład mapowania całego modelu
+        public async Task<IActionResult> AddOrEditUser(int id, [Bind("Name", "Password")] User user) //przykład użycia atrybutu Bind do ograniczenia mapowania tylko do wybranych właściwości modelu
+        {
+            if (id == 0)
+            {
+                await _service.Create(user);
+            }
+            else
+            {
+                await _service.UpdateAsync(user.Id, user);
+            }
             return RedirectToAction("Index");
         }
 
