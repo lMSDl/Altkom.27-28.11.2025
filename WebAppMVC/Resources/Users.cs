cb0e670cfa5fe50acbc161c495ffa0602bc37a72
Paweł <pawel@msd.info.pl>
2025-11-28 11:04:26 +0100

    Zasoby i lokalizacje - komentarze

--- WebAppMVC/Resources/Users.cs
+++ WebAppMVC/Resources/Users.cs
@@ -1,5 +1,9 @@
 ﻿namespace WebAppMVC.Resources
 {
+    //Klasa zasobów do internacjonalizacji związana z użytkownikami
+    //pusty plik stosowany do lokalizacji zasobów w katalogu Resources wymagany dla StringLocalizer i HtmlLocalizer
+    //Domowym katalogiem zasobów jest katalog Resources, więc plik ten informuje Localizery aby szukać zasobów w katalogu Resources
+    //Dlatego zasoby umieszczone są w Resources/Resources (katalog domowy + katalog umiejsciowenia pliku .cs)
     public class Users
     {
     }
